95fe758c7fe1ec4b7d503a1a60d02bd0c9c34688
antswartz <aswartz@rackspace.com>
2014-11-24 10:34:02 -0600

    added the systemy packages

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'apache'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'I Can Haz Devops'
+maintainer_email 'aswartz@rackspace.com'
 license          'All rights reserved'
-description      'Installs/Configures apache'
+description      'Installs/Configures apache and other tidbits'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,16 +7,36 @@
 # All rights reserved - Do Not Redistribute
 #
 
+
 package 'httpd' do
   action :install
 end
-
-
 
 service 'httpd' do 
   action [ :enable, :start ] 
 end
 
+package 'screen' do
+  action :install
+end
+
+package 'ruby' do
+  action :install
+end
+
+package 'curl' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+package 'vim-enhanced' do
+  action :install
+end
+
+#Calls the index file for Apache server
 
 cookbook_file '/var/www/html/index.html' do 
   source 'index.html' 
